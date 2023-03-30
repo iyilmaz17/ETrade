eb0b6b01090f03a870da98b908496030e9648830
İsmail <iylmz1707@gmail.com>
2023-03-30 12:40:04 +0300

    Added Validation Architecture

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(5);
+
+            // UnitPrice
+            RuleFor(p => p.UnitPrice).GreaterThan(0).WithMessage("Ürün fiyatı 0 olamaz");
+            RuleFor(p => p.UnitPrice).NotEmpty();
+        }
+    }
+}
